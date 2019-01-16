c5286a2158dd9410fa72c33172ff4dd94b2b0ad7
方永乐 <fangyongle@innobuddy.com>
2019-01-16 17:16:03 +0800

    feat: update doc

---
+++ index.js
@@ -0,0 +1,87 @@
+const Table = require('cli-table');
+const chalk = require('chalk');
+
+const table = new Table({
+  style: {
+    border: ['yellow', 'yellow', 'yellow', 'yellow']
+  },
+  colWidths: [2, 20, 60, 2],
+  colAligns: ['left', 'right', 'left', 'right', 'left'],
+  chars: {
+    top: '═',
+    'top-mid': '═',
+    'top-left': '╔',
+    'top-right': '╗',
+    bottom: '═',
+    'bottom-mid': '═',
+    'bottom-left': '╚',
+    'bottom-right': '╝',
+    left: '║',
+    'left-mid': '║',
+    mid: ' ',
+    'mid-mid': ' ',
+    right: '║',
+    'right-mid': '║',
+    middle: ' '
+  }
+});
+
+table.push(
+  ['', '', '', ''],
+  ['', chalk.cyanBright('Name'), chalk.magenta('Fang Yongle'), ''],
+  ['', chalk.cyanBright('Gender'), chalk.magenta('Male'), ''],
+  ['', chalk.cyanBright('WeChat ID'), chalk.magenta('skylor_fyl'), ''],
+  ['', chalk.cyanBright('Location'), chalk.magenta('Hangzhou'), ''],
+  [
+    '',
+    chalk.cyanBright('Target'),
+    chalk.magenta('Frontend Developer | Scrum Master | Team Leader'),
+    ''
+  ],
+  [
+    '',
+    chalk.cyanBright('Main Tech'),
+    chalk.magenta('Js, React, WeChat Miniprogram, Vue, NodeJs, etc.'),
+    ''
+  ],
+  [
+    '',
+    chalk.cyanBright('Version Control'),
+    chalk.magenta('Git (Gitlab, Github, etc.)'),
+    ''
+  ],
+  [
+    '',
+    chalk.cyanBright('Bio'),
+    chalk.magenta('Designer, Frontend Developer & overall web enthusiast'),
+    ''
+  ],
+  ['', chalk.cyanBright('Home'), chalk.green('https://fangyongle.com/'), ''],
+  [
+    '',
+    chalk.cyanBright('Github'),
+    chalk.green('https://github.com/skyFi/'),
+    ''
+  ],
+  [
+    '',
+    chalk.cyanBright('Github Orgs'),
+    chalk.green('https://github.com/kiruya/'),
+    ''
+  ],
+  [
+    '',
+    chalk.cyanBright('CNBlog'),
+    chalk.green('https://www.cnblogs.com/skylor/'),
+    ''
+  ],
+  [
+    '',
+    chalk.cyanBright('Hack Resume'),
+    chalk.green('https://hacknical.com/skyFi/resume?locale=zh'),
+    ''
+  ],
+  ['', '', '', '']
+);
+
+console.log(table.toString());
---
+++ readme.md
@@ -0,0 +1,27 @@
+```
+             Name   Fang Yongle
+
+           Gender   Male
+
+        WeChat ID   skylor_fyl
+
+         Location   Hangzhou
+
+           Target   Frontend Developer | Scrum Master | Team Leader
+
+        Main Tech   Js, React, WeChat Miniprogram, Vue, NodeJs, etc.
+
+  Version Control   Git (Gitlab, Github, etc.)
+
+              Bio   Designer, Frontend Developer & overall web enthusiast
+
+             Home   [https://fangyongle.com/](https://fangyongle.com/)
+
+           Github   [https://github.com/skyFi/](https://github.com/skyFi/)
+
+      Github Orgs   [https://github.com/kiruya/](https://github.com/kiruya/)
+
+           CNBlog   [https://www.cnblogs.com/skylor/](https://www.cnblogs.com/skylor/)
+
+      Hack Resume   [https://hacknical.com/skyFi/resume?locale=zh](https://hacknical.com/skyFi/resume?locale=zh)
+```
